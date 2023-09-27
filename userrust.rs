8492b2b8ba2805ec552e1d18956494512c69363a
Bienn Viquiera <biennviquiera@gmail.com>
2023-09-27 03:55:09 +0000

    add print

--- userrust.rs
+++ userrust.rs
@@ -16,7 +16,7 @@ fn main() -> io::Result<()> {
         let mut writer = BufWriter::new(&file);
         writer.write_all(b"f bruh")?;
     } // writer goes out of scope and flushes automatically
-
+    println!("write success");
     file.seek(SeekFrom::Start(0))?;
     let mut buffer = [0; 18];
     let out = file.read(&mut buffer)?;
