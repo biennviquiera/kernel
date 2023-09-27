6653c9d11a338dfd9dfe8bd8676db8a2eafbe04c
Bienn Viquiera <biennviquiera@gmail.com>
2023-09-27 17:09:09 +0000

    new test

--- userrust.rs
+++ userrust.rs
@@ -1,29 +1,38 @@
 use std::fs::File;
-use std::io::Read;
-use std::io::{self, BufWriter, Write, Seek, SeekFrom};
+use std::io::{self, BufWriter, Write, Read, Seek, SeekFrom};
 
 const DEVICE_PATH: &str = "/dev/mymem";
 
 fn main() -> io::Result<()> {
     let mut file = File::options()
-    .read(true)
-    .write(true)
-    .open(DEVICE_PATH)?;
-
+        .read(true)
+        .write(true)
+        .open(DEVICE_PATH)?;
 
     println!("Device opened successfully.");
+
+    // First write
     {
         let mut writer = BufWriter::new(&file);
-        writer.write_all(b"f bruh")?;
+        writer.write_all(b"First write: Hello, kernel!")?;
     } // writer goes out of scope and flushes automatically
-    println!("write success");
+
     file.seek(SeekFrom::Start(0))?;
-    let mut buffer = [0; 18];
+    let mut buffer = [0; 256];
     let out = file.read(&mut buffer)?;
-
     let result_str = String::from_utf8(buffer[..out].to_vec()).unwrap();
+    println!("Received from kernel after first write: {}", result_str);
 
-    println!("Received from kernel: {}", result_str);
+    // Second write
+    {
+        let mut writer = BufWriter::new(&file);
+        writer.write_all(b"Second write: Hello again, kernel!")?;
+    } // writer goes out of scope and flushes automatically
+
+    file.seek(SeekFrom::Start(0))?;
+    let out = file.read(&mut buffer)?;
+    let result_str = String::from_utf8(buffer[..out].to_vec()).unwrap();
+    println!("Received from kernel after second write: {}", result_str);
 
     Ok(())
 }
